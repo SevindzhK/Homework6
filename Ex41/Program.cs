e0d69637756351a71a3ba8e8bff08bf7a3749620
SevindzhK <sevinge0709@gmail.com>
2022-09-22 16:58:01 +0300

    Added Ex41

---
+++ Ex41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Enter the amount of numbers to check: ");
+int amount = Convert.ToInt32(Console.ReadLine()); 
+
+int CheckNumbersMoreThanZero(int amt)
+{
+    int count = 0;
+    
+    for (int i = 0; i < amount; i++)
+    {
+        Console.WriteLine("Enter the number: ");
+        int number = Convert.ToInt32(Console.ReadLine()); 
+        if (number > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine($"The amount of numbers greater zero: {CheckNumbersMoreThanZero(amount)}");
+
+
